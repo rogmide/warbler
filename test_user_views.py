c6278ce976e54ca4b24bfa6cb637dedb192e31c8
Roger Delgado <rogmide@gmail.com>
2022-09-08 18:21:27 -0400

    working testing the views for user

--- test_user_views.py
+++ test_user_views.py
@@ -1,3 +1,4 @@
+
 import os
 from unittest import TestCase
 from models import db, connect_db, Message, User, Likes, Follows
@@ -21,7 +22,7 @@ class UserViewTestCase(TestCase):
         self.user1 = User.signup("user1", "user1@test.com", "123456", None)
         self.user2 = User.signup("user2", "user2@test.com", "123456", None)
         self.user3 = User.signup("user3", "user3@test.com", "123456", None)
-        
+
         db.session.commit()
 
     def tearDown(self):
@@ -29,16 +30,36 @@ class UserViewTestCase(TestCase):
         db.session.rollback()
 
     def test_users(self):
+        '''Test /users page to see if properlly display the 3 users'''
         with app.test_client() as client:
             resp = client.get("/users")
 
             self.assertIn("@user1", str(resp.data))
             self.assertIn("@user2", str(resp.data))
             self.assertIn("@user3", str(resp.data))
-    
-    # def test_show_user(self):
-    #     with app.test_client() as client:
-    #         resp = client.get(f"/users/")
 
-    #         self.assertEqual(resp.status_code, 200)
-    #         # self.assertIn("@user1", str(resp.data))
+    def test_user_need_signup(self):
+        '''Making sure that the user has to be sign up first to see user profile'''
+
+        with app.test_client() as client:
+            resp = client.get("/users/profile", follow_redirects=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn(
+                "Sign up now to get your own personalized timeline!", str(resp.data))
+
+    def test_users_search(self):
+        '''Testing search for users'''
+
+        with app.test_client() as client:
+            resp = client.get("/users?q=user")
+
+            self.assertNotIn("NoIn", str(resp.data))
+            self.assertIn("@user1", str(resp.data))
+            self.assertIn("@user3", str(resp.data))            
+            
+
+
+
+
+
