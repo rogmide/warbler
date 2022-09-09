8d28afbef2f4a98390d2494e2f52dc8b46c659d9
Roger Delgado <rogmide@gmail.com>
2022-09-09 18:32:48 -0400

    finish FT main page likes

--- static/javascript/app.js
+++ static/javascript/app.js
@@ -1,31 +1,29 @@
 // Handle add like to a msg
-$(".add-like").on("click", async function (e) {
+$(".button-action").on("click", async function (e) {
   e.preventDefault();
-  let msg_id = $(this).attr("id");
-  alert("add");
-  const new_like = await axios.post(`/users/add_like/${msg_id}`, {
-    action: "add",
-  });
 
-  if (new_like.data.result == "add_pass") {
-    $(this).toggleClass("add-like remove-like");
-    $(`.${msg_id}-icon_likes`).toggleClass("fa-heart  fa-thumbs-up");
-    $(`#${msg_id}`).toggleClass("btn-primary  btn-secondary");
-  }
-});
+  const $this = $(this);
+  let msg_id = $this.attr("id");
 
-// handle remove like to a msg
-$(".remove-like").on("click", async function (e) {
-  e.preventDefault();
-  alert("remove");
-  let msg_id = $(this).attr("id");
-  const new_like = await axios.post(`/users/add_like/${msg_id}`, {
-    action: "remove",
-  });
+  if ($this.hasClass("add-like")) {
+    const new_like = await axios.post(`/users/add_like/${msg_id}`, {
+      action: "add",
+    });
 
-  if (new_like.data.result == "remove_pass") {
-    $(this).toggleClass("remove-like add-like");
-    $(`.${msg_id}-icon_likes`).toggleClass("fa-thumbs-up fa-heart");
-    $(`#${msg_id}`).toggleClass("btn-secondary btn-primary");
+    if (new_like.data.result == "add_pass") {
+      $this.toggleClass("add-like remove-like");
+      $(`.${msg_id}-icon_likes`).toggleClass("fa-heart  fa-thumbs-up");
+      $(`#${msg_id}`).toggleClass("btn-primary  btn-secondary");
+    }
+  } else {
+    const new_like = await axios.post(`/users/add_like/${msg_id}`, {
+      action: "remove",
+    });
+
+    if (new_like.data.result == "remove_pass") {
+      $this.toggleClass("remove-like add-like");
+      $(`.${msg_id}-icon_likes`).toggleClass("fa-thumbs-up fa-heart");
+      $(`#${msg_id}`).toggleClass("btn-secondary btn-primary");
+    }
   }
 });
