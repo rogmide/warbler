5485bc63305acb0be35c17023c77b4b9c02a4d51
Roger Delgado <rogmide@gmail.com>
2022-09-09 18:43:40 -0400

    small change to add performance

--- static/javascript/app.js
+++ static/javascript/app.js
@@ -1,10 +1,9 @@
-// Handle add like to a msg
+// Handle Add or Remove Likes for the user
+
 $(".button-action").on("click", async function (e) {
   e.preventDefault();
-
   const $this = $(this);
   let msg_id = $this.attr("id");
-
   if ($this.hasClass("add-like")) {
     const new_like = await axios.post(`/users/add_like/${msg_id}`, {
       action: "add",
